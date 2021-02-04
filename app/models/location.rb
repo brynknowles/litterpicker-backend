d6ac0f9a2a5f60204859b4eeb7575ed3f469bf45
brynknowles <bryn.knowles@gmail.com>
2021-02-03 22:07:36 -0500

    model relationships done

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,5 @@
 class Location < ApplicationRecord
+    has_many :cleanups
+    has_many :users, through: :cleanups
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :cleanups
+    has_many :locations, through: :cleanups
 end
