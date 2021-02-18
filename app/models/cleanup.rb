7e0553e61a59198068f7f2cb087a2c38fc67d58b
brynknowles <bryn.knowles@gmail.com>
2021-02-18 13:39:39 -0500

    attendee signup works

--- app/models/cleanup.rb
+++ app/models/cleanup.rb
@@ -1,4 +1,8 @@
 class Cleanup < ApplicationRecord
   has_many :user_cleanups, dependent: :destroy
   has_many :users, through: :user_cleanups
+
+  def get_user_cleanups 
+    self.user_cleanups
+  end
 end
--- app/serializers/cleanup_serializer.rb
+++ app/serializers/cleanup_serializer.rb
@@ -1,5 +1,5 @@
 class CleanupSerializer < ActiveModel::Serializer
-  attributes :id, :name, :location, :category, :image, :date, :start_time, :end_time, :comment, :cheer, :user_cleanups
+  attributes :id, :name, :location, :category, :image, :date, :start_time, :end_time, :comment, :cheer, :get_user_cleanups 
   has_many :user_cleanups
   has_many :users, through: :user_cleanups
 end
