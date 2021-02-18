22785ef816ce82adb99547a47d4196377e9a102b
brynknowles <bryn.knowles@gmail.com>
2021-02-18 11:20:08 -0500

    serializers change

--- app/serializers/cleanup_serializer.rb
+++ app/serializers/cleanup_serializer.rb
@@ -1,5 +1,5 @@
 class CleanupSerializer < ActiveModel::Serializer
-  attributes :id, :name, :location, :category, :image, :date, :start_time, :end_time, :comment, :cheer
+  attributes :id, :name, :location, :category, :image, :date, :start_time, :end_time, :comment, :cheer, :user_cleanups
   has_many :user_cleanups
   has_many :users, through: :user_cleanups
 end
--- app/serializers/user_cleanup_serializer.rb
+++ app/serializers/user_cleanup_serializer.rb
@@ -1,5 +1,3 @@
 class UserCleanupSerializer < ActiveModel::Serializer
-  attributes :id
-  belongs_to :user
-  belongs_to :cleanup
+  attributes :id, :user, :cleanup
 end
